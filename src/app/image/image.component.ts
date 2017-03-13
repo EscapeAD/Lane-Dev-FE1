f0a7ebf42abe3d62331bd2d81a6c9ea855a58f50
Adam Tse <mr.adam.tse@gmail.com>
2017-03-13 00:55:05 -0400

    ngClass Fail

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,60 +1,17 @@
-import { Component, OnInit, animate, state, style, transition, trigger } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ImageResult, ResizeOptions } from 'ng2-imageupload';
 
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.css'],
-  animations: [trigger(
-    'rotating',
-    [
-      state('rot', style({transform: 'rotate(45deg)'})),
-      state('rotTran', style({transform: 'rotate(45deg) translateX(-40px)'})),
-      state('rotScale', style({transform: 'rotate(45deg) scale(0.5)'})),
-      state('rotTranScal', style({transform: 'rotate(45deg) translateX(-40px) scale(0.5)'})),
-      state('none', style({transform: 'rotate(0deg)'})),
-      // transition(
-      //     '* <=> *', [animate(500, style({transform: 'rotate(45deg)'})), animate(500)])
-    ]),
-
-    trigger(
-    'translating',
-    [
-      state('tran', style({transform: 'translateX(-40px)'})),
-      state('rotTran', style({transform: 'rotate(45deg) translateX(-40px)'})),
-      state('rotTranScal', style({transform: 'rotate(45deg) translateX(-40px) scale(0.5)'})),
-      state('tranScale', style({transform: 'translateX(-40px) scale(0.5)'})),
-      state('none', style({transform: 'translateX(0px)'})),
-      // transition(
-      //     '* <=> *', [animate(500, style({transform: 'translateX(-40px)'})), animate(500)])
-    ]),
-    trigger(
-    'scaling',
-    [
-      state('true', style({transform: 'scale(0.5)'})),
-      transition(
-          '* <=> *', [animate(500, style({ transform: 'scale(0.5)' })), animate(500)])
-    ]),
-    trigger('opaciting', [
-    // What happens when toggleState is true
-      state('true' , style({ opacity: '0.5' })),
-      // What happens when toggleState is false
-      state('false', style({})),
-      transition(
-          '* <=> *', [animate(500, style({ opacity: '0.5' })), animate(500)])
-    ])
-
-
-  ]
 })
 export class ImageComponent implements OnInit {
 src: string = ""
 // Rotate
 rotateStat:   boolean = false
-rotString:    string  = "none"
 // translate
-tranlateStat: boolean = false
-tranlateString: string = 'none'
+translateStat: boolean = false
 // Scale
 scaleStat:    boolean = false
 // opactiy
@@ -77,31 +34,9 @@ opacityStat:  boolean = false
 
   rotate(){
     this.rotateStat = (this.rotateStat === false ? true : false);
-    if(this.rotateStat && this.tranlateStat && this.scaleStat){
-      this.rotString = 'rotTranScal'
-    } else if (this.rotateStat && this.tranlateStat) {
-      this.rotString = 'rotTran'
-    } else if (this.rotateStat && this.scaleStat) {
-      this.rotString = 'rotScale'
-    } else if(this.rotateStat) {
-      this.rotString = 'rot'
-    } else if(!this.rotateStat){
-      this.rotString = 'none'
-    }
   }
   translate(){
-    this.tranlateStat = (this.tranlateStat === false ? true : false);
-    if(this.rotateStat && this.tranlateStat && this.scaleStat){
-      this.tranlateString = 'rotTranScal'
-    } else if (this.rotateStat && this.tranlateStat) {
-      this.tranlateString = 'rotTran'
-    } else if (this.tranlateStat && this.scaleStat) {
-      this.rotString = 'tranScale'
-    } else if(this.rotateStat) {
-      this.tranlateString = 'tran'
-    } else if(!this.rotateStat){
-      this.tranlateString = 'none'
-    }
+    this.translateStat = (this.translateStat === false ? true : false);
   }
   scale(){
     this.scaleStat = (this.scaleStat === false ? true : false);
