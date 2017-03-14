da58bb57648ef9f85c6f0b9d78575f8421739530
Adam Tse <mr.adam.tse@gmail.com>
2017-03-14 03:58:53 -0400

    added shadow and minor fixes

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 import { ImageResult, ResizeOptions } from 'ng2-imageupload';
 
 @Component({
@@ -6,8 +6,9 @@ import { ImageResult, ResizeOptions } from 'ng2-imageupload';
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.css'],
 })
-export class ImageComponent implements OnInit {
+export class ImageComponent {
 src: string = ""
+transformation: string = ""
 // Rotate
 rotateStat:   boolean = false
 // translate
@@ -16,11 +17,10 @@ translateStat: boolean = false
 scaleStat:    boolean = false
 // opactiy
 opacityStat:  boolean = false
+// opactiy
+shadowStat:  boolean = false
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
   resizeOptions: ResizeOptions = {
       resizeMaxHeight: 400,
@@ -34,14 +34,49 @@ opacityStat:  boolean = false
 
   rotate(){
     this.rotateStat = (this.rotateStat === false ? true : false);
+    this.transform()
   }
   translate(){
     this.translateStat = (this.translateStat === false ? true : false);
+    this.transform()
   }
   scale(){
     this.scaleStat = (this.scaleStat === false ? true : false);
+    this.transform()
   }
   opacity(){
     this.opacityStat = (this.opacityStat === false ? true : false);
+  }
+  shadow(){
+    this.shadowStat = (this.shadowStat === false ? true : false);
+  }
+
+  transform(){
+    if(this.scaleStat && this.rotateStat && this.translateStat){
+      this.transformation  = 'rotate(45deg) scale(0.5) translateX(40px)'
+    } else if(this.scaleStat && this.rotateStat){
+      this.transformation  =  'rotate(45deg) scale(0.5)'
+    } else if(this.scaleStat && this.translateStat){
+      this.transformation  =  'translateX(40px) scale(0.5)'
+    } else if(this.rotateStat && this.translateStat){
+      this.transformation  =  'translateX(40px) rotate(45deg)'
+    } else if(this.rotateStat){
+      this.transformation  =  'rotate(45deg)'
+    } else if(this.translateStat){
+      this.transformation  =  'translateX(40px)'
+    } else if(this.scaleStat){
+      this.transformation  =  'scale(0.5)'
+    } else {
+      this.transformation  = ""
+    }
+  }
+  reset(){
+    this.src             = ""
+    this.transformation  = ""
+    this.rotateStat      = false
+    this.translateStat   = false
+    this.scaleStat       = false
+    this.opacityStat     = false
+    this.shadowStatS     = false
   }
 }
